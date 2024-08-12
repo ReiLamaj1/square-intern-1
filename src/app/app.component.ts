bb6467856ee660f51251ec60ada2629028aeacbe
ReiLamaj1 <154746091+ReiLamaj1@users.noreply.github.com>
2024-08-12 21:28:37 +0200

    Component 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component2Component } from './component-2/component-2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Component2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'square-intern-1';
+export class AppComponent  implements OnInit{
+  title: string = "Test";
+  constructor(){
+    console.log("Konstruktori");
+  }
+  ngOnInit(): void {
+      console.log("OnInit");
+  }
 }
---
+++ src/app/component-2/component-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'square-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-2.component.html',
+  styleUrl: './component-2.component.css'
+})
+export class Component2Component {
+
+}
